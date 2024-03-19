930eb6c6ce8e6ac604e56a4cd9d36675805aef69
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-19 14:21:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Sets up details about a smartphone
+var smartphone = {
+    make: "Infinix",
+    model: "Hot 30i",
+    specs: {
+        storage: "130GB",
+        screenSize: "6.2 inches",
+        batteryLife: "20 hours"
+    }
+};
+// Shows what we've set up about the smartphone
+console.log(smartphone);
---
+++ app.ts
@@ -0,0 +1,13 @@
+// Sets up details about a smartphone
+let smartphone = {
+    make: "Infinix",
+    model: "Hot 30i",
+    specs: {
+        storage: "130GB",
+        screenSize: "6.2 inches",
+        batteryLife: "20 hours"
+    }
+};
+
+// Shows what we've set up about the smartphone
+console.log(smartphone);
